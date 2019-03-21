96b9624c0ef33770312e1d0abaa7ed387d5d968d
Paskahlis Anjas <pjaswo1204@gmail.com>
2019-03-21 13:10:21 +0700

    initial commit

---
+++ lib/main.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(
+    MaterialApp(debugShowCheckedModeBanner: false, home: DebtRecorder()));
+
+class DebtRecorder extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() => DebtRecorderState();
+}
+
+class DebtRecorderState extends State<DebtRecorder> {
+  @override
+  Widget build(BuildContext context) {}
+}
